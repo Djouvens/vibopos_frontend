0a2c7151bda30511268eca3414ca46a4751bfb62
Cepoudy Djouvens <ingcepoudy@gmail.com>
2025-10-14 12:42:34 -0400

    FIRST PUSH BACKEND VIBOPOS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+  return (
+    <>
+     <Button> vibopos system</Button>
+    </>
+  )
+}
+
+export default App
